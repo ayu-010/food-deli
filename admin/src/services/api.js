4f3fa7dfb36f9b9bf13427ebf0446d0dcae531a3
ayu-010 <aj3157182@gmail.com>
2024-06-26 23:30:11 +0530

    first commit

--- admin/src/services/api.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL
-
-// AUTH ENDPOINTS
-export const endpoints = {
-
-  ADD_PRODUCT_API: BASE_URL + "/add",
-LIST_PRODUCT_API:BASE_URL+  "/list",
-DELETE_PRODUCT_API:BASE_URL+ "/remove"
-}
